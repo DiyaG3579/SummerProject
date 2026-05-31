f9424a64dbc304e26b1e82fd0b85984fcfbcb4f9
DiyaG3579 <diya@wustl.edu>
2026-05-31 14:10:38 -0500

    Omg omg It worked!

---
+++ LED.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+
+#Intial setup and defining all the pins
+GPIO.setmode(GPIO.BOARD)
+r = 11
+g = 12
+b = 13
+
+#Actual setup for the PI and starting them all as off
+GPIO.setup(r, GPIO.OUT)
+GPIO.setup(g, GPIO.OUT)
+GPIO.setup(b, GPIO.OUT)
+GPIO.output(r, GPIO.LOW)
+GPIO.output(g, GPIO.LOW)
+GPIO.output(b, GPIO.LOW)
+
+#Now it gets fun - lighting them up
+
+GPIO.output(r, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(g, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(r, GPIO.LOW)
+time.sleep(1)
+GPIO.output(b, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(g, GPIO.LOW)
+time.sleep(1)
+GPIO.output(r, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(g, GPIO.HIGH)
+time.sleep(1)
+
+
+GPIO.cleanup()
